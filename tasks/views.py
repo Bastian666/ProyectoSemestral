5b97e486bf637c1dcb3ecb8d5a9c4762618d2b04
Bastián <bast.guzman@duocuc.cl>
2024-05-14 12:20:42 -0400

    Actualización de funcionalidades del CRUD para el administrador
    Se desarrollaron todas las funcionalidades correspondientes al CRUD del administrador, permitiendo agregar productos con todas sus características correspondientes y agregar imágenes junto al producto, de igual forma modificarlo y eliminarlo.

--- tasks/views.py
+++ tasks/views.py
@@ -89,7 +89,7 @@ def detalleProd(request, producto_id):
     else:
         try:
             producto = get_object_or_404(Productos, pk=producto_id)
-            form = ProductoForm(request.POST, instance=producto)
+            form = ProductoForm(request.POST, request.FILES, instance=producto)
             form.save()
             return redirect('productos')
         except ValueError:
